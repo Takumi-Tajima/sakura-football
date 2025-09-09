d431b00e18c7ff0afa83b6412696995c49d16074
taji <taji@sonicgarden.jp>
2025-09-09 15:38:06 +0900

    Userモデル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :administrators, controllers: { sessions: 'administrators/sessions' }
   get 'up' => 'rails/health#show', as: :rails_health_check
 
