916231dad02a16a6b445306d46b231c3fa4b8c6b
taji <taji@sonicgarden.jp>
2025-10-16 15:20:31 +0900

    fixbit

--- app/controllers/administrators/bookings_controller.rb
+++ app/controllers/administrators/bookings_controller.rb
@@ -1,5 +1,5 @@
 class Administrators::BookingsController < Administrators::ApplicationController
   def index
-    @bookings = Booking.includes(:lesson, :user).default_order
+    @bookings = Booking.includes(:lesson, :user).upcoming.default_order
   end
 end
